bc1d4bc7721f33dcf3ea7b5244d732c5bc6c0d6f
Nicolas Wormser <nicolas@codewithflair.org>
2023-06-22 10:37:47 +0200

    Complete express 2

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,6 +17,11 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
@@ -22,3 +29,4 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
